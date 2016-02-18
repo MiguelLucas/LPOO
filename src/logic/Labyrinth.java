4cc8a6d9cdd084d7809ef23f6e6c5e8461fef6df
Miguel Lucas <cmiguel.lucas@gmail.com>
2016-02-18 11:29:00 +0000

    Initial project

---
+++ src/logic/Labyrinth.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class Labyrinth {
+
+	public Labyrinth() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
