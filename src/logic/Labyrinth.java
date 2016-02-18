7339fca9461a50f0c3e50387a44d399821316aa8
Miguel Lucas <cmiguel.lucas@gmail.com>
2016-02-18 11:33:44 +0000

    teste

--- src/logic/Labyrinth.java
+++ src/logic/Labyrinth.java
@@ -3,11 +3,11 @@ package logic;
 public class Labyrinth {
 
 	public Labyrinth() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Cenas");
 
 	}
 
